5f051e50f2c499e7794d02ebe4cd88f7d880d345
Philip Krause <pkrause@exzeo.com>
2023-06-20 20:57:14 -0500

    Changes unrelated to a JIRA task

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './sw';
+import registerServiceWorker from './registerServiceWorker';
 import history from './history'
 
 
