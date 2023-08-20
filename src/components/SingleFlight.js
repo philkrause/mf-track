5f922c1b9af0f570f68ffa8648a8ef34ee1862f2
pkrause <pkrause@exzeo.com>
2023-08-20 02:10:25 -0500

    changes

--- src/components/SingleFlight.js
+++ src/components/SingleFlight.js
@@ -116,7 +116,7 @@ export default function MapSetHooks(props) {
     { lat: 0, lon: 0 }
   )
   const flightICAO = props.match.params
-  console.log(`DataKey: ${flightICAO}`)
+  console.log(`DataKey: ${json.stringify(flightICAO)}`)
   const dataKey = `${flightICAO}-data`
 
   const [viewport, setViewPort] = useState({
